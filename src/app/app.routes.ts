00a95dd8bf3fe54fe40f71e5c3001d104b2aa5e0
Miguel A. Pardo Alvarez <miguelpardoalvare50@gmail.com>
2024-11-19 12:15:41 -0500

    Configuraciones en archivos App.x

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListarFestivosComponent } from './features/componentes/listar-festivos/listar-festivos.component';
+import { ValidarFestivosComponent } from './features/componentes/validar-festivos/validar-festivos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "listar", component: ListarFestivosComponent },
+    { path: "validar", component: ValidarFestivosComponent },
+];
