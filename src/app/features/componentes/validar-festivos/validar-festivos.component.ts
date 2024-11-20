8616496edf31a99ae4d4518f63eaee31f899c873
Miguel A. Pardo Alvarez <miguelpardoalvare50@gmail.com>
2024-11-19 20:08:03 -0500

    Mejoras en Validar

--- src/app/features/componentes/validar-festivos/validar-festivos.component.ts
+++ src/app/features/componentes/validar-festivos/validar-festivos.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
+import { FormsModule } from '@angular/forms';
+import { FestivoService } from '../../servicios/festivo.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-validar-festivos',
   standalone: true,
-  imports: [],
+  imports: [
+    ReferenciasMaterialModule,
+    FormsModule
+  ],
   templateUrl: './validar-festivos.component.html',
   styleUrl: './validar-festivos.component.css'
 })
+
 export class ValidarFestivosComponent {
 
+  fechaSeleccionada: Date | null = null;
+  resultado: String | null = null;
+
+  public festivos: String = "";
+
+  constructor(private festivoServicio: FestivoService, private dialogoServicio: MatDialog) { }
+  
+  public validarFestivo() {
+    if (this.fechaSeleccionada) {
+      const dia = this.fechaSeleccionada.getDate();
+      const mes = this.fechaSeleccionada.getMonth() + 1;
+      const año = this.fechaSeleccionada.getFullYear();
+  
+      this.festivoServicio.validar(dia, mes, año).subscribe({
+        next: (resultado: String) => {
+          window.alert(resultado); // Maneja la respuesta como texto
+        },
+        error: (error) => {
+          window.alert('Error al validar la fecha: ' + error.message);
+        },
+      });
+    } else {
+      window.alert('Por favor selecciona una fecha.');
+    }
+  }
 }
